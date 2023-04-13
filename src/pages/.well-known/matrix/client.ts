f6da507a36dafaba3105dbcbd89dd2b152e66f0f
franchb <hello@francb.com>
2023-04-13 19:23:23 +0300

    fix: clean

--- src/pages/.well-known/matrix/client.ts
+++
@@ -1,8 +0,0 @@
-export function get() {
-  const root: any = {
-    "m.homeserver": { base_url: "https://matrix.franchb.com:8448" },
-  };
-  return {
-    body: JSON.stringify(root),
-  };
-}
--- src/pages/.well-known/matrix/server.ts
+++
@@ -1,6 +0,0 @@
-export function get() {
-  const root: any = { "m.server": "matrix.franchb.com:8448" };
-  return {
-    body: JSON.stringify(root),
-  };
-}
