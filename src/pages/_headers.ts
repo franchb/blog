37ca4f3fbe7e78a007895c6925e575cf0b0c5469
franchb <hello@francb.com>
2023-04-13 09:07:41 +0300

    fix(cloudflare): add _headers for .well-known/matrix/*

--- src/pages/_headers.ts
+++
@@ -1,9 +0,0 @@
-export function get() {
-  return {
-    body:
-      ".well-known/matrix/*\n" +
-      "  X-Frame-Options: DENY\n" +
-      "  Access-Control-Allow-Origin: *\n" +
-      "  Content-Type: application/json",
-  };
-}
