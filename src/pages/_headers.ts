d0481267e18bf1d9c878954b3b8ebe95704bf2ca
franchb <hello@francb.com>
2023-04-13 06:40:50 +0300

    fix(deps): update dependencies

--- src/pages/_headers.ts
+++ src/pages/_headers.ts
@@ -1,8 +1,9 @@
 export function get() {
-  const root: any = {
-    "m.homeserver": { base_url: "https://matrix.franchb.com:8448" },
-  };
   return {
-    body: JSON.stringify(root),
+    body:
+      ".well-known/matrix/*\n" +
+      "  X-Frame-Options: DENY\n" +
+      "  Access-Control-Allow-Origin: *\n" +
+      "  Content-Type: application/json",
   };
 }
