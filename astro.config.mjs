23e112a1701190d00dbd726e2127da008418e899
franchb <hello@francb.com>
2023-09-17 17:19:41 +0300

    fix(build): await in missed func

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,32 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import markdoc from "@astrojs/markdoc";
+import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
+
+// enable to turn on SSR -- import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  site: "https://franchb.com/",
+  build: {
+    inlineStylesheets: "auto",
+  },
+  redirects: {
+    "/old": "/new",
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
     sitemap(),
+    markdoc(),
   ],
   markdown: {
     remarkPlugins: [
       remarkToc,
+      remarkReadingTime,
       [
         remarkCollapse,
         {
@@ -36,5 +48,8 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"],
     },
   },
+  // In order to enable Cloudflare SSR/Functions see https://github.com/satnaing/astro-paper/issues/44
+  // output: "server",
+  // TODO: switch to a directory mode for Sentry adapter: cloudflare({ mode: "directory" }),
   scopedStyleStrategy: "where",
 });
